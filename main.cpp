cf921eb0a6c2340fe209492afb6005cdac9a9835
HtonS <reshodko@gmail.com>
2019-04-26 15:57:27 +0900

    Read the data

--- main.cpp
+++ main.cpp
@@ -1,28 +1,35 @@
 #include <iostream>
+#include <string>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
+#include "easyloggingcpp/easylogging++.h"
+
+using namespace std;
 
 int main (int argc, char** argv)
 {
+  // Load configuration from file
+  el::Configurations conf("src/logConfig.config");
+  // Reconfigure single logger
+  el::Loggers::reconfigureLogger("default", conf);
+  // Check command line for log-relevant flags
+  START_EASYLOGGINGPP(argc, argv);
+  // Log program title
+  LOG(INFO) << argv[0] << " has started successfully\n";
+
   pcl::PointCloud<pcl::PointXYZ> cloud;
-
-  // Fill in the cloud data
-  cloud.width    = 5;
-  cloud.height   = 1;
-  cloud.is_dense = false;
-  cloud.points.resize (cloud.width * cloud.height);
-
-  for (size_t i = 0; i < cloud.points.size (); ++i)
+  // Load data
+  string dataF = "data/20160426_CALIB.txt";
+  if (pcl::io::load(dataF, cloud) == -1)
   {
-    cloud.points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
-    cloud.points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
-    cloud.points[i].z = 1024 * rand () / (RAND_MAX + 1.0f);
+    LOG(ERROR) << "Cannot load file " << dataF << "\n";
+    return -1;
   }
 
   //pcl::io::savePCDFileASCII ("test_pcd.pcd", cloud);
   //std::cerr << "Saved " << cloud.points.size () << " data points to test_pcd.pcd." << std::endl;
 
-  for (size_t i = 0; i < cloud.points.size (); ++i)
+  for (size_t i = 0; i < 5; ++i)
     std::cerr << "    " << cloud.points[i].x << " " << cloud.points[i].y << " " << cloud.points[i].z << std::endl;
 
   return (0);
